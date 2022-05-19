4d44bbed5b5274bd11b12b0631c3a79582441ff7
AVKostin <alexander.kostin.ua@gmail.com>
2022-05-19 20:28:45 +0300

    Create Notification.styled.jsx

---
+++ src/components/Notification/Notification.styled.jsx
@@ -0,0 +1,11 @@
+import styled from '@emotion/styled';
+
+export const NotificationText = styled.p`
+  display: flex;
+  align-items: center;
+  font-size: 20px;
+  font-weight: 500;
+  & span {
+    margin-right: 20px;
+  }
+`;
